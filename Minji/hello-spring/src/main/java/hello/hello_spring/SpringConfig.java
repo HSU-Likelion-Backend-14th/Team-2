1e6992143858aab247dce962b74cbce569bd598b
mint0326 <jmj200436@hansung.ac.kr>
2026-03-26 04:06:25 +0900

    [섹션 7] 24~25. JPA/스프링데이터JPA

--- Minji/hello-spring/src/main/java/hello/hello_spring/SpringConfig.java
+++ Minji/hello-spring/src/main/java/hello/hello_spring/SpringConfig.java
@@ -1,9 +1,11 @@
 package hello.hello_spring;
 
 import hello.hello_spring.repository.JdbcTemplateMemberRepository;
+import hello.hello_spring.repository.JpaMemberRepository;
 import hello.hello_spring.repository.MemberRepository;
 import hello.hello_spring.repository.MemoryMemberRepository;
 import hello.hello_spring.service.MemberService;
+import jakarta.persistence.EntityManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,22 +13,31 @@ import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
+//    private final DataSource dataSource;
+//    private final EntityManager em;
+    private final MemberRepository memberRepository;
 
-    private final DataSource dataSource;
-
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    /*
+        public SpringConfig(DataSource dataSource, EntityManager em) {
+            this.dataSource = dataSource;
+            this.em = em;
+        }
+    */
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
-
+/*
     @Bean
     public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
-        return new JdbcTemplateMemberRepository(dataSource);
+//        return new JdbcTemplateMemberRepository(dataSource);
+//        return new JpaMemberRepository(em);
     }
+*/
 }
