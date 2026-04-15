5def91aa7b2dd87bb8575987e4942989e3281065
Yoon <koscom@Yoon-MacBook-Pro.local>
2026-04-15 18:00:08 +0900

    feat: Entity 코드작성

---
+++ Jungyoon/week4/demo/src/main/java/com/example/demo/entity/Comment.java
@@ -0,0 +1,31 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Table (name="comments")
+@Entity
+@Getter
+@Setter
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer commentId;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private Integer content;
+
+    @Column(name="created_at", updatable = false, columnDefinition =" TIMESTAMP_DEFAULT_CURRENT_TIMESTAMP")
+    private LocalDateTime createdAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name ="post_id")
+    private Post post;
+}
