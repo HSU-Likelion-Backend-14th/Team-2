404272b93653e9ecdbcfadab39e8222e7f7d58f7
anrwkd9902 <anrwkd9902@naver.com>
2026-04-16 20:24:20 +0900

    feat: Like Entity 추가

---
+++ Jongwook/week4/sns/src/main/java/com/likelion/sns/entity/Like.java
@@ -0,0 +1,30 @@
+package com.likelion.sns.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "comments")
+@Getter
+@Setter
+
+public class Like {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer like_id;
+
+    @Column(name = "created_at", updatable = false,
+    columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private LocalDateTime createdAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+}
