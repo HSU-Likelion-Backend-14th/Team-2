6740e3670cbc70460112509e729a6b40178fa386
anrwkd9902 <anrwkd9902@naver.com>
2026-05-07 22:51:11 +0900

    feat: UserSaveReq 코드 작성

---
+++ Jongwook/week6/demo/src/main/java/com/likelion/demo/domain/user/web/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.likelion.demo.domain.user.web.controller;
+
+import com.likelion.demo.domain.user.service.UserService;
+import com.likelion.demo.global.constant.response.code.SuccessResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
+public class UserController {
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<SuccessResponse<?>> saveUser (
+        @Valid @RequestBody UserSaveReq req)
+    ){
+        userService.save(req);
+        return ResponseEntity.status(HttpStatus.CREATED).body(SuccessResponse.empty());
+    }
+}
