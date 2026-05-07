92c0589b1c91e799335967e70d6469c8f18255e0
Jungyoon <yuny0519@gmail.com>
2026-05-07 16:49:23 +0900

    feat: User 엔티티 구현 (#19)

---
+++ Jungyoon/week6/User.java
@@ -0,0 +1,21 @@
+package com.Likelion.demo.domain.user.entity;
+
+import com.Likelion.demo.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class User extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+}
